17ed6c6e75d30880ab0a4b742188b1acda689a5f
Jwseph <116055108+jwsephdev@users.noreply.github.com>
2026-01-01 12:24:59 +0800

    1/1/26
    cleaned and fixed stuff

--- TP-Controller/Scripts/player.gd
+++ TP-Controller/Scripts/player.gd
@@ -1,8 +1,17 @@
 class_name Player extends CharacterBody3D
 
+
+# remove having multiple collision shapes and just change the shape of 1 coll shape 
+#
+# var shape = CylinderShape.new()
+# shape.set_radius(new_radius)
+# shape.set_height(new_height)
+# $Spatial/CollisionShape.shape = shape
+
+
 @export_category("Movement")
-@export var player_standspeed :float  = 8.0
-@export var player_crouchspeed :float = 4.0
+@export var standspeed :float  = 8.0
+@export var crouchspeed :float = 4.0
 
 @export_category("Transitions")
 @export var lerp_speed :float= 10.0
@@ -10,13 +19,17 @@ class_name Player extends CharacterBody3D
 @export_category("Camera")
 @export var cam_sens :float= 0.1
 
-@export_category("Crouching")
-@export var mount_pos_crouch :Vector3= Vector3(0,-0.5,0)
-@export var springlength_crouch :float= 2.0
+@export_subgroup("Distance Transparency")
+@export var cam_transp_amount:float = 0.8
+@export var cam_transp_mindis:float = 1.8
 
-@export_category("Standing")
-@export var springlength_stand :float= 5.0
-@export var mount_pos_stand :Vector3= Vector3(0,0.5,0)
+@export_subgroup("Crouching")
+@export var cam_pos_crouch :Vector3= Vector3(0.4,-0.5,0)
+@export var cam_dist_crouch :float= 2.0
+
+@export_subgroup("Standing")
+@export var cam_pos_stand :Vector3= Vector3(0.15,0.2,0)
+@export var cam_dist_stand :float= 4.0
 
 var playerSpeed :float
 var direction = Vector3.ZERO
@@ -25,6 +38,7 @@ var is_crouching:bool = false
 const JUMP_VELOCITY :float= 4.5
 
 @onready var visuals: Node3D = $playerVisuals
+
 @onready var camera_mount: Node3D = $cameraMount
 @onready var camera: Camera3D = $cameraMount/cameraSpringarm/camera
 @onready var camera_springarm: SpringArm3D = $cameraMount/cameraSpringarm
@@ -43,12 +57,22 @@ func camera_controls(event):
 		camera_mount.rotate_x(deg_to_rad( -event.relative.y) * cam_sens)
 		camera_mount.rotation.x = clamp(camera_mount.rotation.x, deg_to_rad(-70), deg_to_rad(15))
 
+func player_cam_transparency(delta):
+	if snapped(camera_springarm.get_hit_length(),0.01) <= cam_transp_mindis:
+		
+		player_crouching_mesh.transparency = lerp(player_crouching_mesh.transparency,cam_transp_amount,delta*lerp_speed)
+		player_standing_mesh.transparency = lerp(player_standing_mesh.transparency,cam_transp_amount,delta*lerp_speed)
+	else:
+		player_crouching_mesh.transparency = lerp(player_crouching_mesh.transparency,0.0,delta*lerp_speed)
+		player_standing_mesh.transparency = lerp(player_standing_mesh.transparency,0.0,delta*lerp_speed)
+	
 func crouching(delta):
 	if is_crouching:
-		playerSpeed = player_crouchspeed
+		if is_on_floor():
+			playerSpeed = crouchspeed
 		
-		camera_mount.position = lerp(camera_mount.position, mount_pos_crouch, delta*lerp_speed)
-		camera_springarm.spring_length = lerp(camera_springarm.spring_length, springlength_crouch, delta*lerp_speed)
+		camera_mount.position = lerp(camera_mount.position, cam_pos_crouch, delta*lerp_speed)
+		camera_springarm.spring_length = lerp(camera_springarm.spring_length, cam_dist_crouch, delta*lerp_speed)
 		
 		player_crouching_col.disabled = false
 		player_standing_col.disabled = true
@@ -56,11 +80,10 @@ func crouching(delta):
 		player_crouching_mesh.show()
 		player_standing_mesh.hide()
 	else:
-		playerSpeed = player_standspeed
+		playerSpeed = standspeed
 		
-		camera_mount.position = lerp(camera_mount.position, mount_pos_stand, delta*lerp_speed)
-		camera_springarm.spring_length = lerp(camera_springarm.spring_length, springlength_stand, delta*lerp_speed)
-		
+		camera_mount.position = lerp(camera_mount.position, cam_pos_stand, delta*lerp_speed)
+		camera_springarm.spring_length = lerp(camera_springarm.spring_length, cam_dist_stand, delta*lerp_speed)
 		player_crouching_col.disabled = true
 		player_standing_col.disabled = false
 		
@@ -74,14 +97,19 @@ func crouching(delta):
 		if is_crouching and !Input.is_action_pressed("Crouch"):
 			is_crouching = false
 
-	
-func _ready() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-
 func _input(event: InputEvent) -> void:
 	camera_controls(event)
+	if Input.is_action_pressed("show_cursor"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	else:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _process(delta: float) -> void:
+	crouching(delta)
+	player_cam_transparency(delta)
 
 func _physics_process(delta: float) -> void:
+	
 	$fpslabel.text = "fps: " + str(Engine.get_frames_per_second())
 	
 	if not is_on_floor():
@@ -106,6 +134,5 @@ func _physics_process(delta: float) -> void:
 	else:
 			velocity.x = lerp(velocity.x, direction.x * playerSpeed, delta*5.0)
 			velocity.z = lerp(velocity.z, direction.z * playerSpeed, delta*5.0)
-		
-	crouching(delta)
+	
 	move_and_slide()
